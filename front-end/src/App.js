bd6c61fb6ac1d7321306e92beedf067996769dab
Allan Wazacz <cazwazacz@gmail.com>
2018-01-26 16:49:39 +0000

    Create search box which makes api request when search button is clicked

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,12 +6,38 @@ import MovieList from './components/MovieList/MovieList';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { movieList: null };
+    this.state = { movieList: null, searchText: null };
   }
 
   componentDidMount() {
     let self = this;
     fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=121486b23802e0b6735125ff1892f340')
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+          self.setState({ movieList: data.results })
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+
+  searchText(event) {
+    this.setState( { searchText: event.target.value } );
+  }
+
+  getSearch() {
+    let self = this;
+    let url = 'https://api.themoviedb.org/3/search/movie?query=' + this.state.searchText + '&sort_by=popularity.desc&api_key=121486b23802e0b6735125ff1892f340';
+    fetch(url)
     .then(
       function(response) {
         if (response.status !== 200) {
@@ -41,6 +67,8 @@ class App extends Component {
     } else {
       return (
         <div className="App">
+          <input type="text" onChange={(event) => this.searchText(event)} />
+          <button onClick={() => this.getSearch()}> Search! </button>
           {movies}
         </div>
       )
