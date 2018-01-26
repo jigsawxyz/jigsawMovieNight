29a92b34960a332dfeda406878eff318af61fa35
cristhiandas <cristhiandf94@gmail.com>
2018-01-26 14:49:49 +0000

    add test dependencies

--- front-end/__tests__/functions.tests.js
+++ front-end/__tests__/functions.tests.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { sayHello } from '../src/services/stateFunctions';
+import { mount } from 'enzyme';
 
 test('greets the makers', () => {
   const greeting = sayHello();
