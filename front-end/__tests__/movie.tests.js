da6812c72d619e794bc1e9bcb1c95d8fe56aaa17
cristhiandas <cristhiandf94@gmail.com>
2018-01-26 15:58:03 +0000

    created tests for the movie component

---
+++ front-end/__tests__/movie.tests.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Movie from '../src/components/Movie/Movie';
+import '../src/setupTest'
+
+describe('movie', () => {
+  // let movie = mount(<Movie />);
+
+  const props = {title:'title', overview:'overview', path:'path'}
+  let wrapper = shallow(<Movie />);
+
+  it('renders without crashing', () => {
+    expect(wrapper.type()).toEqual('div');
+  })
+
+  let wrapper2 = mount(<Movie {...props} />);
+
+  it('have a title', () => {
+    expect(wrapper2.find('p').at(0).text()).toEqual(props.title)
+  })
+
+  it('have an overview', () => {
+    expect(wrapper2.find('p').at(1).text()).toEqual(props.overview)
+  })
+
+  it('have a path', () => {
+    expect(wrapper2.find('p').at(2).text()).toEqual(props.path)
+  })
+})
