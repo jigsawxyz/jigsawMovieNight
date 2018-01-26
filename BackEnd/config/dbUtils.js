5562e8015c36fd106882004efa25d819990f66a6
Allan Wazacz <cazwazacz@gmail.com>
2018-01-26 17:07:29 +0000

    Connect to development database

--- BackEnd/config/dbUtils.js
+++ BackEnd/config/dbUtils.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const monk = require('monk');
 
 // Connection URL
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/movie_development';
 
 const db = monk(url);
 
@@ -13,4 +13,3 @@ db.then(() => {
 module.exports = {
   db,
 };
-
